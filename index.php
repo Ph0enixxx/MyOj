a5eb62d528df1e05e68b6c90fcdec703b940c008
shaoyingshuai <754406613@qq.com>
2015-11-27 16:49:47 +0800

    增加了刷新过快的限制 防止有人恶意刷新

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
 <?php
+ 
+error_reporting(0);  
+if($_COOKIE["ck"])die("刷新过快！");  
+//if($_COOKIE["ck"])header("Location:http://www.baidu.com");//这里如果用户刷新过快，给予终止php脚本或者直接302跳转  
+setcookie("ck","1",time()+1);//设定cookie存活时间3s  
+//echo "hello!"; 
 
 if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
 	define('APP_DEBUG',True);
