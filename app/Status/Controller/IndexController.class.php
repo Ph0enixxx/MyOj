881f8d8f6fc414903d48aa8c431b8351b7b0021d
shaoyingshuai <754406613@qq.com>
2016-05-27 19:47:47 +0800

    status 后台

--- app/Status/Controller/IndexController.class.php
+++ app/Status/Controller/IndexController.class.php
@@ -2,7 +2,20 @@
 namespace Status\Controller;
 use Think\Controller;
 class IndexController extends Controller {
-    public function index(){
-        $this->show('<style type="text/css">*{ padding: 0; margin: 0; } div{ padding: 4px 48px;} body{ background: #fff; font-family: "微软雅黑"; color: #333;font-size:24px} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.8em; font-size: 36px } a,a:hover,{color:blue;}</style><div style="padding: 24px 48px;"> <h1>:)</h1><p>欢迎使用 <b>ThinkPHP</b>！</p><br/>版本 V{$Think.version}</div><script type="text/javascript" src="http://ad.topthink.com/Public/static/client.js"></script><thinkad id="ad_55e75dfae343f5a1"></thinkad><script type="text/javascript" src="http://tajs.qq.com/stats?sId=9347272" charset="UTF-8"></script>','utf-8');
+    public function index($page=1){
+    	$m = M('solution');
+    	//按条件查找
+    	$tmp = [];
+    	$tmp['user_id'] = I('get.user_id');
+    	$tmp['problem_id'] = I('get.problem_id');
+    	$tmp['language'] = I('get.language');
+    	$tmp['judge_time'] = I('get.judge_time');
+    	//分页！
+    	//var_dump($tmp);
+    	if($tmp['user_id']||$tmp['problem_id']||$tmp['language']||$tmp['judge_time'])
+    		$data = $m->where($tmp)->page($page,10)->select();
+    	else
+    		$data = $m->page($page,10)->select();
+    	var_dump($data);
     }
 }
