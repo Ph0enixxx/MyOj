23ad9735029e816abba6a3e5fc87a3a4792038ef
shaoyingshuai <754406613@qq.com>
2016-06-05 10:39:14 +0800

    problem

--- app/Home/Controller/IndexController.class.php
+++ app/Home/Controller/IndexController.class.php
@@ -6,19 +6,4 @@ class IndexController extends Controller {
     {
         $this->display();
     }
-    public function Problem($page = 1)
-    {
-        //prblem data
-        $d = [];
-        $data['id']='1001';
-        $data['title']='a+b';
-        $data['source']='sys';
-        $data['AC']='12';
-        $data['submit']='1200';
-        $d[0] = $data;
-        $this->problemList = $d;
-        $this->P = 4;
-        $this->display('problem');
-        
-    }
 }
--- app/Problem/Controller/IndexController.class.php
+++ app/Problem/Controller/IndexController.class.php
@@ -2,7 +2,33 @@
 namespace Problem\Controller;
 use Think\Controller;
 class IndexController extends Controller {
-    public function index(){
-        
+    public function index()
+    {
+        #总列表contest表
+        $m = M('contest');
+        $data = $m->order("start_time desc")->select();
+        var_dump($data);
+        $this->data = $data;
+        $this->display();
+    }
+    public function inside($cid=0)
+    {
+    	#一级目录contest_problem表
+    	$m = M('contest_problem');
+    	$tmp = [];
+    	$tmp['contest_id'] = $cid;
+        $data = $m->field('problem_id')->select();
+        var_dump($data);
+        $this->data = $data;
+        $this->display();
+    }
+    public function view($pid='')
+    {
+    	$m = M('problem');
+        $data = $m->select();
+        var_dump($data);
+        $this->data = $data;
+        $this->display();
+    	#单个问题probelm表
     }
 }
